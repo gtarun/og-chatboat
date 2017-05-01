580f8748b8beaf0ef0310d2395afa995d5bf5152
Biplab Malakar <biplab.x@venturepact.com>
2017-05-01 12:41:00 -0700

    Maste Branch Update with new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,30 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './share/header/header.component';
-import { StartPageComponent } from './start-page/start-page.component';
-
-import {ImageServicesService} from './services/image-services.service';
-import {ReactiveFormsModule} from '@angular/forms';
-import { FacebookInteComponent } from './facebook-inte/facebook-inte.component';
-
+import { FacebookAppComponent } from './facebook-app/facebook-app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    StartPageComponent,
-    FacebookInteComponent
+    FacebookAppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    ReactiveFormsModule,
+    HttpModule
   ],
-  providers: [ImageServicesService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/facebook-app/facebook-app.component.ts
+++ src/app/facebook-app/facebook-app.component.ts
@@ -1,35 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { FacebookInitParams, FacebookLoginOptions, FacebookService, FacebookLoginResponse } from 'ng2-facebook-sdk';
-import {Http, Response} from '@angular/http';
+import {Http, Response, Headers} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 import  'rxjs/Rx';
 
 @Component({
-  selector: 'app-facebook-inte',
-  templateUrl: './facebook-inte.component.html',
-  styleUrls: ['./facebook-inte.component.css'],
+  selector: 'app-facebook-app',
+  templateUrl: './facebook-app.component.html',
   providers: [FacebookService]
 })
-export class FacebookInteComponent implements OnInit {
-
+export class FacebookAppComponent implements OnInit {
 showDiv: boolean = false;
 tok: String = '';
-appId: string = '792069617611842';
 bipId: string = '206683399815932';
 pageId: string = '';
 pageToken: string = '';
 pageSubscribeUrl: string = '';
 user_pages = [];
 user_label_share = [];
+routeRoot = 'https://og-chat-bot.herokuapp.com';
   constructor(public fb: FacebookService, public http: Http) { 
     let fbParams : FacebookInitParams = {
-        appId: this.appId,
+        appId: this.bipId,
         xfbml: true,
         version: 'v2.8'
     };
     this.fb.init(fbParams);
   }
-
   ngOnInit() {
   }
 
@@ -37,7 +34,7 @@ user_label_share = [];
    this.fb.login({scope: 'email, manage_pages, read_page_mailboxes,publish_pages, pages_messaging'}).then(
       (response: FacebookLoginResponse) =>{
         let url = 'https://graph.facebook.com/'+response.authResponse.userID+'/accounts?access_token='+response.authResponse.accessToken;
-        console.log('User Access Token: ', response.authResponse.accessToken);
+        console.log('User Access Token: ',response.authResponse.userID, response.authResponse.accessToken);
         this.http.get(url).map((res,Response)=> res.json())
           .catch(err=> err.json())
           .subscribe(data=>{
@@ -62,22 +59,45 @@ user_label_share = [];
 
 connectPage(item, i){
   console.log('Select Page is: ', item.name, item.id, i);
-  //access_token="+item.access_token;
   console.log('Page Subscribe Url: ', this.pageSubscribeUrl);
   this.pageToken = item.access_token;
   this.pageId = item.id;
   var self = this;
-  //let access_token = "206683399815932|ehMr4fL3JC7HJsoqeMKg9TWctAw";
-  //let access_token = "792069617611842|vuCRMvbZcZhv0OslCzkpoTgCUqQ";
-  //console.log('Access Token: ', self.tok);
+  let access_token = "206683399815932|ehMr4fL3JC7HJsoqeMKg9TWctAw";
   this.pageSubscribeUrl = "https://graph.facebook.com/v2.8/me/subscribed_apps?access_token="+ this.pageToken;
-  let urlS="https://graph.facebook.com/"+this.pageId+"/subscriptions?object=page&callback_url=https://og-chatbot-api.herokuapp.com/userwebhook&fields=messages&verify_token=outgrow_messanger_bot_verify_code_user&access_token=792069617611842|vuCRMvbZcZhv0OslCzkpoTgCUqQ";
+  let urlS="https://graph.facebook.com/"+this.pageId+"/subscriptions?object=page&callback_url=" + this.routeRoot + "/api/v1/messenger/userwebhook&fields=messages&verify_token=outgrow_messanger_bot_verify_code_user&access_token="+ access_token;
   console.log('URL IS: ', urlS);
   this.http.post(urlS, {})
     .subscribe(response=>{
       console.log('Webhook Subscribed Data: ', response);
-      this.http.get(this.pageSubscribeUrl)
-        .subscribe(data=> console.log('Page Subscribe Data: ', data), err=> console.log('Page Subscribe Error: ', err))
+      this.http.post(this.pageSubscribeUrl, {})
+        .subscribe(data=> {
+          console.log('Page Subscribe Data: ', data);
+          let header= new Headers();
+          header.append('Content-Type', 'application/json');
+          let getSatrtButtonUrl = 'https://graph.facebook.com/v2.8/'+this.pageId + '/thread_settings?access_token='+this.pageToken;
+          let dt = {
+             "setting_type":"call_to_actions",
+              "thread_state":"new_thread",
+              "call_to_actions":[
+                {
+                  "payload":"Start Now"
+                }
+              ]
+          };
+          this.http.post(getSatrtButtonUrl, dt, {headers: header})
+          .subscribe(res=>{
+            console.log('Start Button: ', res);
+            let head = new Headers();
+            head.append('Content-Type', 'application/json');
+            this.http.post(this.routeRoot + '/api/v1/messenger/pageinfo', {pageId: this.pageId, accessToken: this.pageToken}, {headers: head})
+            .subscribe(res=> console.log('Access token and Page Id Send')
+            , err=> console.log('AccessToken and PageId Not Send', err))
+          }, err=>{
+            console.log('Get Start Error: ', err);
+          })
+        }
+        ,err=> console.log('Page Subscribe Error: ', err))
     }, err=>{
       console.log('Webhook Subscribed Error: ', err);
   })
@@ -85,4 +105,5 @@ connectPage(item, i){
  openWindow(){
    window.open("https://www.facebook.com/pages", '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
  }
+
 }
